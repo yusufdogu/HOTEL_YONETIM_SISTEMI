d505b9778dc1d64c164d4103b6b675adf332ed72
MOHAMMED ABDULHAMID ALBAWENDI <107199115+mohammed2004bawendi@users.noreply.github.com>
2024-12-09 10:45:11 +0300

    Update RezervasyonSilEkrani.java

--- HotelManagementGui/src/RezervasyonSilEkrani.java
+++ HotelManagementGui/src/RezervasyonSilEkrani.java
@@ -1,7 +1,7 @@
 import javax.swing.*;
 
 public class RezervasyonSilEkrani extends JFrame {
-    public RezervasyonSilEkrani() {
+    public RezervasyonSilEkrani(JFrame previousFrame, OtelMisafirYonetici misafirYonetici) {
         setTitle("Rezervasyon Sil");
         setSize(300, 200);
         setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
@@ -11,22 +11,39 @@ public class RezervasyonSilEkrani extends JFrame {
         JLabel rezervasyonNoLabel = new JLabel("Rezervasyon No:");
         JTextField rezervasyonNoField = new JTextField();
         JButton silButton = new JButton("Sil");
+        JButton geriButton = new JButton("GERİ");
 
-        rezervasyonNoLabel.setBounds(20, 20, 150, 30);
-        rezervasyonNoField.setBounds(170, 20, 100, 30);
-        silButton.setBounds(100, 80, 100, 30);
+        rezervasyonNoLabel.setBounds(20, 20, 120, 30);
+        rezervasyonNoField.setBounds(150, 20, 100, 30);
+        silButton.setBounds(50, 80, 80, 30);
+        geriButton.setBounds(150, 80, 80, 30);
 
         add(rezervasyonNoLabel);
         add(rezervasyonNoField);
         add(silButton);
+        add(geriButton);
 
+        // Silme işlemi
         silButton.addActionListener(e -> {
-            double rezervasyonNo = Double.parseDouble(rezervasyonNoField.getText());
+            String rezervasyonNo = rezervasyonNoField.getText().trim();
+            if (rezervasyonNo.isEmpty()) {
+                JOptionPane.showMessageDialog(this, "Rezervasyon No alanını doldurun!", "Hata", JOptionPane.ERROR_MESSAGE);
+                return;
+            }
 
-            // Rezervasyon silme işlemi
-            System.out.println("Rezervasyon silindi: " + rezervasyonNo);
+            boolean bulundu = misafirYonetici.silRezervasyon(rezervasyonNo);
+            if (bulundu) {
+                JOptionPane.showMessageDialog(this, "Rezervasyon başarıyla silindi!");
+                rezervasyonNoField.setText("");
+            } else {
+                JOptionPane.showMessageDialog(this, "Rezervasyon bulunamadı!", "Hata", JOptionPane.ERROR_MESSAGE);
+            }
+        });
 
-            // Gerçek rezervasyon silme işlemi veritabanı ile yapılabilir.
+        // GERİ butonu
+        geriButton.addActionListener(e -> {
+            previousFrame.setVisible(true);
+            dispose();
         });
 
         setVisible(true);
